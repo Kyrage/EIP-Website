e29729af4637ddd10bd50bf85e132231d174ec86
Tristan M <tristan.mesurolle@epitech.eu>
2021-07-07 16:45:08 +0200

    [*] Update login password to access to gmail

--- app/core/settings.py
+++ app/core/settings.py
@@ -174,8 +174,9 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'epitech.noreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'Epitech()'
+EMAIL_HOST_PASSWORD = 'pcejukkbasyjtyxy'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
